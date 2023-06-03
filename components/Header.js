de6c2932bb7ec1758d3e12de3e8f167f82366cae
Mert Kırgıl <kirgilmert@gmail.com>
2023-06-03 23:46:41 +0300

    header stillendirme

--- components/Header.js
+++ components/Header.js
@@ -1,22 +1,28 @@
-import Link from "next/link";
+import Button from "./Button";
 import styled from "styled-components";
 
 const HeaderDiv = styled.div`
-  background-color: blue;
+  background-color: #000;
   padding: 30px;
+  display: flex;
+  gap: 20px;
+
+
+  span {
+    font-size: 30px;
+  }
+
 
 `;
 
 
+
 export default () => 
 <HeaderDiv>
-<Link href="/" passHref>
-    Ana Sayfa
-</Link>
+<Button href="/">Ana Sayfa</Button>
+<Button href="/about">Hakkımda</Button>
 
-<Link href="/about" passHref>
-    Hakkımızda
-</Link>
+
 </HeaderDiv>
 
 
