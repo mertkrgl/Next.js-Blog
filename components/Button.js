010d317caa5793cc193c6b3b03294a5c674e7143
Mert Kırgıl <kirgilmert@gmail.com>
2023-06-04 10:41:06 +0300

    0

--- components/Button.js
+++ components/Button.js
@@ -23,29 +23,3 @@ export default ({ href, children }) => {
 
 
 
-
-// import Link from "next/link";
-// import styled from "styled-components";
-
-// const StyledLink = styled(Link)`
-// font-size: 16px;
-// color: #fff;
-// text-decoration: none;
-// &:hover{
-//     color: rgba(255, 255, 255, 0.7);
-// }
-
-// `
-
-
-// const Button = ({href ,children}) => {
-//     return (
-//         <StyledLink href={href} passHref>
-//             {children}
-//         </StyledLink>
-
-//     )
-
-// };
-
-// export default Button
--- components/Header.js
+++ components/Header.js
@@ -30,35 +30,3 @@ export default () => (
 
 
 
-// import Button from "./Button";
-// import styled from "styled-components";
-
-// const HeaderDiv = styled.div`
-//   background-color: #000;
-//   margin: auto;
-
-//   padding: 30px;
-//   display: flex;
-//   gap: 20px;
-
-
-//   span {
-//     font-size: 30px;
-//   }
-
-
-// `;
-
-
-
-// export default () => 
-// <HeaderDiv>
-// <Button href="/">Ana Sayfa</Button>
-// <Button href="/about">Hakkımda</Button>
-// <Button href="/blog">Blog</Button>
-
-
-// </HeaderDiv>
-
-
-
