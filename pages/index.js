06bf73fad988ff02561af2e50f2f0369bb186cf8
Mert Kırgıl <kirgilmert@gmail.com>
2023-06-04 14:15:36 +0300

    ekleme

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,8 @@ export default () => {
     uncover many web sites still in their infancy. Various versions have evolved
     over the years, sometimes by accident, sometimes on purpose (injected humour
     and the like).
+
+    Deneme yazısı
     </Layout>
   )
 }
